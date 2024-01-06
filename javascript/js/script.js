3a4cfbcece3731b1d77d327c6bf8d00556cf228e
farihafari <farihafari594@gmail.com>
2024-01-06 12:58:47 +0500

    js second day lecture

--- javascript/js/script.js
+++ javascript/js/script.js
@@ -1,2 +1,16 @@
+// var x="<h6 style='color:blue;background-color:yellow'>this is javascript</h6>";
+// document.write(x);
+var x = 15;
 
-document.write("<h6 style='color:blue;background-color:yellow'>this is javascript</h6>")
+let y = "fariha";
+
+y=55;
+const z="6";
+let a= true;
+
+let b ;
+document.write ( "<h6 style='color:blue;background-color:yellow'>"+y+" has a "+ typeof y +" datatype</h6>");
+document.write ( "<h6 style='color:blue;background-color:yellow'>"+x+" has a "+ typeof x +" datatype</h6>");
+document.write ( "<h6 style='color:blue;background-color:yellow'>"+z+" has a "+ typeof z +" datatype</h6>");
+document.write ( "<h6 style='color:blue;background-color:yellow'>"+a+" has a "+ typeof a +" datatype</h6>");
+// document.write(z);
