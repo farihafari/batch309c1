0ad4051c2b5d2490e52a2341f72f34b5946792fc
farihafari <farihafari594@gmail.com>
2024-01-09 12:50:03 +0500

    operators

---
+++ javascript/js/operators.js
@@ -0,0 +1,19 @@
+// let number1 = 5;
+// let number2 = 9;
+// let number3 = "9";
+// let  result = number2+number3
+// // document.write( number1+number2+" is the addition of number1 and number2 <br>"); 
+// // document.write( " is the subtraction of number2 and number3 "+number2-number3); 
+// let userName=prompt("enter your name");
+// document.write(userName);
+// alert(userName);
+// user detail
+let name = prompt("enter your name");
+let email = prompt("enter your email");
+let qualification = prompt("enter your qualification");
+let workExperience= prompt("enter your work experience");
+document.write("<h1>hello, dear ",name," </h1>");
+document.write("<h2>your email is ",email," </h2>");
+document.write("<h3>your qualification is ",qualification," </h3>");
+document.write("<h4>your work experience is ",workExperience," years</h4>");
+
