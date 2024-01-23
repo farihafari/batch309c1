1e4af8af323ddefaeb25eab8803c5b011d819978
farihafari <farihafari594@gmail.com>
2024-01-23 12:56:03 +0500

    while

---
+++ javascript/js/switch.js
@@ -0,0 +1,24 @@
+let day = prompt("enter the day for class");
+switch(day){
+    case "monda`y":
+        alert(day +"! no this is not your class ");
+        break;
+        case "tuesday":
+            alert(day +"! yes this is your class ");
+            break;
+            case "wednesday":
+                alert(day +"! no this is not your class ");
+            break;
+            case "thursday":
+            alert(day +"! yes this is your class ");
+            break;
+            case "friday":
+                alert(day +"! no this is not your class ");
+            break;
+            case "saturday":
+            alert(day +"! yes this is your class ");
+            break;
+            default:
+                alert("please select any working day "); 
+          break;
+}
---
+++ javascript/js/while.js
@@ -0,0 +1,7 @@
+// while loop
+let i=1;
+while(i<10){
+
+    document.write("while loop count "+i+"<br>");
+i++;
+}
