95bec385b58f36cf2f64f46f1c2ae9ffb632779b
farihafari <farihafari594@gmail.com>
2024-03-07 12:59:00 +0500

    product page start

---
+++ booststrap/btproject/assets/images/js/accessories.js
@@ -0,0 +1,96 @@
+let accessories = {
+    usb : [
+        {
+            name:"Original USB",
+            price:157,
+            describtion:"100% Original USB A + Type C Cable For All Phones Flash Charger Cable Super Flash Charging Type C Charger Wire for All Android Phones - 1 meter",
+            image:"assets/images/accessories/u1.webp"
+
+        },
+        {
+            name:"EZone 8-Pin",
+            price:399,
+            describtion:"EZone 8-Pin Connector to 3.5mm Audio Headphone Jack Adapter 2 in 1 | Aux Dual Jack Splitter | Compatible iPhone Connector Converter Lightning to 3.5mm | Handfree earphone Splitter & Converter for iPhone 7/ 7+/ 8/ 8+/ X/ XS Max/ 11/ 12/ 13/ 14/ Pro/ Max",
+            image:"assets/images/accessories/u2.webp"
+
+        },
+        {
+            name:"3 in 1 kFast Charging Data Cable",
+            price:298,
+            describtion:"100% Original Imported 3 in 1 kFast Charging Data Cable (Iphone, Type C and Micro Usb Data Cable) 60 watt,",
+            image:"assets/images/accessories/u3.webp"
+
+        },
+    ],
+    phonecases:[
+        {
+            name:"Wave Clear Silicone Phone Case",
+            price:359,
+            describtion:"Wave Clear Silicone Phone Case For IPhone 11 12 13 14 15 Pro Max 6 6s 7 8 Plus Se 2020 XS Max XS XR Covers Shockproof Phone Casing Shell",
+            image:"assets/images/accessories/p1.webp"
+        },
+        {
+            name:"Anti Shock Corner",
+            price:133,
+            describtion:"Infinix Smart 8 Anti Shock Corner Bumper 1.5mm Soft Transparent Jelly Cover",
+            image:"assets/images/accessories/p2.webp"
+
+        },
+    ],
+    stand:[
+        {
+            name:"Phone Holder Stand",
+            price:245,
+            describtion:"ALOGY Mobile Phone Desk Phone Holder Stand For Mobile Adjustable Desktop Holder Universal Table Cell Phone Stand",
+            image:"assets/images/accessories/s1.webp"
+
+        },
+        {
+            name:"Stand Bracket Holder",
+            price:249,
+            describtion:"Foldable Desktop Mobile Phone Stand Bracket Holder / Mobile Phone Stand Mount Universal Adjustable Desktop Smart Phone Tablet Holder Bracket (Black - White)",
+            image:"assets/images/accessories/s2.webp"
+
+        },
+        {
+            name:"charging stand remote holder",
+            price:115,
+            describtion:"Mobile charging stand remote holder self adhesive wall mounted pen holder, Mobile accessories phone holder",
+            image:"assets/images/accessories/s3.webp"
+
+        },
+    ],
+    powerbank:[
+        {
+            name:" Power Bank compatible",
+            price:475,
+            describtion:"Latest Ultra Slim Pocket Size Power Bank compatible with micro USB Ports",
+            image:"assets/images/accessories/pr1.webp"
+
+        },
+        {
+            name:"Keychain Power Bank Mini Device ",
+            price:859,
+            describtion:"Portable Emergency Keychain Power Bank Mini Device | 1500mAh Type-C Ultra-Compact Iphone Mini Battery Fast Charging Backup Power Bank Best Keychain Gift",
+            image:"assets/images/accessories/pr2.webp"
+
+        },
+    ],
+    protect:[
+        {
+            name:"Front & Back Clear Jelly Protector",
+            price:445,
+            describtion:"Infinix Zero 30 4G Front & Back Clear Jelly Protector Soft Film Protection Hydrogel Film Back Protector + Back Rear Camera Protector 9H Gorilla Flexible Protection For Zero 30 4G",
+            image:"assets/images/accessories/sp1.webp"
+
+        },
+        {
+            name:"Protector HD Edge to Edge Full",
+            price:259,
+            describtion:"Google Pixel 4 XL Screen Glass Protector HD Edge to Edge Full Glue Tempered Glass For Pixel 4XL - Black",
+            image:"assets/images/accessories/sp2.webp"
+
+        }
+    ]
+}
+           
