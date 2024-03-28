277a0803eea688ee6454f559c16060a6a99c6bb8
farihafari <farihafari594@gmail.com>
2024-03-28 11:13:07 +0500

    json

--- javascript/panel/registration.js
+++ javascript/panel/registration.js
@@ -4,14 +4,22 @@ function registration(){
     let uPassword = document.querySelector("#uPassword").value;
     let uPhone = document.querySelector("#uPhone").value;
     if(uName && uEmail && uPassword && uPhone){
-
-localStorage.setItem("name",uName);
-localStorage.setItem("email",uEmail);
-localStorage.setItem("password",uPassword);
-localStorage.setItem("phone",uPhone);
+let dataObject ={
+    name:uName,
+    email:uEmail,
+    password:uPassword,
+    phone:uPhone
+}
+// localStorage.setItem("name",uName);
+// localStorage.setItem("email",uEmail);
+// localStorage.setItem("password",uPassword);
+// localStorage.setItem("phone",uPhone);
+localStorage.setItem("userData",JSON.stringify(dataObject))
 alert("registration is compeleted");
 location.assign("login.html");
     }else{
         alert('every field is required to fill ')
     }
 }
+// console.log()
+// document.querySelector("#ab").innerHTML=JSON.parse(localStorage.getItem("userData")).name
